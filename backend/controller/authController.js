1826cb804a0f9b1e6d8ab860e2f67c3f23927ab2
Rajeev6091 <79652796+Rajeev6091@users.noreply.github.com>
2025-08-26 12:30:05 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -23,8 +23,8 @@ export const registration = async (req, res) => {
         let token = await gentoken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -48,8 +48,8 @@ export const login = async (req, res) => {
         let token = await gentoken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 1000
         })
         return res.status(201).json({ user })
@@ -79,8 +79,8 @@ export const googleLogin = async (req, res) => {
         let token = await gentoken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 1000
         })
         return res.status(200).json({ user })
@@ -98,8 +98,8 @@ export const adminLogin = async (req, res) => {
             let token = await gentoken1(email)
             res.cookie("token", token, {
                 httpOnly: true,
-                secure: false,
-                sameSite: "Strict",
+                secure: true,
+                sameSite: "none",
                 maxAge: 1 * 24 * 60 * 1000
             })
             return res.status(200).json(token)
